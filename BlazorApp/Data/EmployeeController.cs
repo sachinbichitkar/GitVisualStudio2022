55420259e14a77035810e4a69f9f0ac3c7b42bbf
Sachinbichitkar <sachinbichitkar09@gmail.com>
2023-01-29 18:31:32 +0530

    EmpController

---
+++ BlazorApp/Data/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Data
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
